0bed4b5f3432086ccedd9641496d20021d45d4b7
gqqnbig <gqqnb2005@gmail.com>
2018-01-13 13:37:38 -0800

    Now it can call google translation API and translate input file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 namespace Translator
 {
@@ -8,24 +10,81 @@ namespace Translator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string keyValueRule = @"(?<=(?<k>[\w_]+\.\d+\.[td]:0)\s+"")(?<v>.+)(?="")";
 
-            string rule = @"(?<=(?<k>[\w_]+\.\d+\.[td]:0)\s+"")(?<v>.+)(?="")";
+            string fileNamePattern = GetNamedParameter(args, "--fileNamePattern");// ".+?_l_{0}.yml";
 
-            Regex reg = new Regex(rule);
+            string apiToken = GetNamedParameter(args, "--apiToken");
 
-            var content= File.ReadAllText(args[0]);
+            string source = GetNamedParameter(args, "--source");
+            var parts = source.Split(':');
+            if (parts.Length != 2)
+                throw new ArgumentException("The value of source must take form of \"fileName:LanguageName\", where LanguageName is the support language in the translator API, eg. eng:en or whatever:zh");
+            string sourceFileName = parts[0];
+            string sourceLanguage = parts[1];
 
-            var matches=reg.Matches(content);
+            string target = GetNamedParameter(args, "--target");
+            parts = target.Split(':');
+            if (parts.Length != 2)
+                throw new ArgumentException("The value of target must take form of \"fileName:LanguageName\", where LanguageName is the support language in the translator API, eg. eng:en or whatever:zh");
+            string targetFileName = parts[0];
+            string targetLanguage = parts[1];
 
-            reg.Replace(content, m =>
+
+
+            string sourceFilePath = string.Format(fileNamePattern, sourceFileName);
+            Regex reg = new Regex(keyValueRule);
+
+            var content = File.ReadAllText(sourceFilePath);
+
+            WebClient client = new WebClient();
+            var translatedContent = reg.Replace(content, m =>
+              {
+                  //Console.WriteLine($"Key: {m.Groups["k"].Value}, Value: {m.Value}");
+
+                  try
+                  {
+                      var url =$"https://translation.googleapis.com/language/translate/v2/?q={m.Value}&source={sourceLanguage}&target={targetLanguage}&key={apiToken}";
+
+
+
+                      var json = JObject.Parse(client.DownloadString(url));
+
+                      var translatedText = json["data"]["translations"][0]["translatedText"];
+
+                      return (string) translatedText;
+                  }
+                  catch (Exception e)
+                  {
+                      Console.Error.WriteLine("Error in translating: " + e.Message);
+                      return m.Value;
+                  }
+              });
+
+            //Console.WriteLine("Translation finished");
+
+            //Console.WriteLine();
+            Console.WriteLine(translatedContent);
+
+            //Console.ReadKey();
+
+        }
+
+        static string GetNamedParameter(string[] args, string name, string shortName = null)
+        {
+            int p = Array.IndexOf(args, name);
+            if (p == -1)
             {
-                Console.WriteLine($"Key: {m.Groups["k"].Value}, Value: {m.Value}");
-                return m.Value;
-            });
+                if (shortName != null)
+                    return GetNamedParameter(args, shortName);
+                else
+                    return null;
+            }
 
-            Console.ReadKey();
+            if (p == args.Length)
+                throw new ArgumentException($"Option {name} requires a value");
 
+            return args[p + 1];
         }
     }
 }
