bacc2c2fcb466738c0e72eacdbc136d6342d7483
gqqnbig <gqqnb2005@gmail.com>
2018-01-13 15:43:36 -0800

    Implement conservative translation
    Only translate keys existing in diff

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Translator
         static void Main(string[] args)
         {
 
-            string fileNamePattern = GetNamedParameter(args, "--fileNamePattern");// ".+?_l_{0}.yml";
+            string fileNamePattern = GetNamedParameter(args, "--fileNamePattern");
 
 
             string source = GetNamedParameter(args, "--source");
@@ -33,54 +33,126 @@ namespace Translator
             string sourceFilePath = string.Format(fileNamePattern, sourceFileName);
             string targetFilePath = string.Format(fileNamePattern, targetFileName);
 
-            Translate(args, sourceFilePath, sourceLanguage, targetLanguage, targetFilePath);
+            TranslateFile(args, sourceFilePath, sourceLanguage, targetLanguage, targetFilePath);
 
             //Console.ReadKey();
 
         }
 
-        private static void Translate(string[] args, string sourceFilePath, string sourceLanguage, string targetLanguage, string targetFilePath)
+        private static void TranslateFile(string[] args, string sourceFilePath, string sourceLanguage, string targetLanguage, string targetFilePath)
         {
 
             string keyValuePattern = GetNamedParameter(args, "--keyValuePattern");
-            string apiToken = GetNamedParameter(args, "--apiToken");
-            Regex reg = new Regex(keyValuePattern);
+            string keyPattern = GetNamedParameter(args, "--keyPattern");
+            Regex reg = new Regex(string.Format(keyValuePattern, keyPattern));
 
             var content = File.ReadAllText(sourceFilePath);
 
-            WebClient client = new WebClient();
-            var translatedContent = reg.Replace(content, m =>
+
+            var diffContent = GetDiffContent(args);
+            var targetContent = File.ReadAllText(targetFilePath);
+
+
+            var translatedContent = reg.Replace(content, match =>
               {
-                  //Console.WriteLine($"Key: {m.Groups["k"].Value}, Value: {m.Value}");
+                  var key = match.Groups["k"].Value;
+                  Console.WriteLine($"Key: {key}, Value: {match.Value}");
 
-                  try
+
+                  if (diffContent != null)
                   {
-                      var url =$"https://translation.googleapis.com/language/translate/v2/?q={m.Value}&source={sourceLanguage}&target={targetLanguage}&key={apiToken}";
+                      if (diffContent.Contains(key))
+                          return TranslateText(args, sourceLanguage, targetLanguage, match.Value);
+                      else
+                      {
+                          string needle = string.Format(keyValuePattern, key);
+                          var m = Regex.Match(targetContent, needle);
 
+                          if (m.Success)
+                          {
+                              //If the key and value is not changed (not exist in diff), don't have to call translation API.
+                              return m.Value;
+                          }
+                          else
+                          {
+                              Console.Error.WriteLine($"Key \"{key}\" doesn't exist in diff nor in translated file \"{targetFilePath}\". " +
+                                                       "The translated file may be manually modified, the program will translate the value of the key.");
 
+                              return TranslateText(args, sourceLanguage, targetLanguage, match.Value);
+                          }
+                      }
 
-                      var json = JObject.Parse(client.DownloadString(url));
-
-                      var translatedText = json["data"]["translations"][0]["translatedText"];
-
-                      return (string) translatedText;
                   }
-                  catch (Exception e)
-                  {
-                      Console.Error.WriteLine("Error in translating: " + e.Message);
-                      return m.Value;
-                  }
+                  else
+                      return TranslateText(args, sourceLanguage, targetLanguage, match.Value);
               });
 
-            //Console.WriteLine("Translation finished");
-
-            //Console.WriteLine();
             Console.WriteLine(translatedContent);
 
             using (StreamWriter sw = new StreamWriter(targetFilePath))
             {
                 sw.Write(translatedContent);
             }
+        }
+
+        private static string TranslateText(string[] args, string sourceLanguage, string targetLanguage, string text)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+                string apiToken = GetNamedParameter(args, "--apiToken");
+                var url = $"https://translation.googleapis.com/language/translate/v2/?q={text}&source={sourceLanguage}&target={targetLanguage}&key={apiToken}";
+
+
+                var json = JObject.Parse(client.DownloadString(url));
+
+                var translatedText = json["data"]["translations"][0]["translatedText"];
+
+                return (string)translatedText;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error in translating: " + e.Message);
+                return text;
+            }
+        }
+
+        private static string GetDiffContent(string[] args)
+        {
+            string diffPath = GetNamedParameter(args, "--diff");
+            string diffContent = null;
+            if (diffPath == null)
+            {
+                if (Console.IsInputRedirected)
+                {
+                    using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+                    {
+                        //Ignore first 4 lines.
+                        sr.ReadLine();
+                        sr.ReadLine();
+                        sr.ReadLine();
+                        sr.ReadLine();
+
+                        diffContent = sr.ReadToEnd();
+                    }
+                }
+            }
+            else
+            {
+                using (StreamReader sr = new StreamReader(diffPath))
+                {
+                    //Ignore first 4 lines.
+                    sr.ReadLine();
+                    sr.ReadLine();
+                    sr.ReadLine();
+                    sr.ReadLine();
+
+                    diffContent = sr.ReadToEnd();
+                }
+            }
+
+            return diffContent;
         }
 
         static string GetNamedParameter(string[] args, string name, string shortName = null)
