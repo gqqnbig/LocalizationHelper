a8e0abea47db638645c81b07a5224b76e35f8c64
gqqnbig <gqqnb2005@gmail.com>
2018-02-04 12:15:04 -0800

    Add IntelliTest project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Translator.Tests")]
+[assembly: InternalsVisibleTo("Translator.Explorables")]
+
